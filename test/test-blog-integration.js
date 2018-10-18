73b4b88552d08cd545b1453ee656bfcc0ccf0c0c
Marissa Faulkner <marissagrace808@gmail.com>
2018-10-17 19:20:23 -0700

    added seed data functions

--- test/test-blog-integration.js
+++ test/test-blog-integration.js
@@ -14,3 +14,30 @@ const {app, runServer, closeServer} = require('../server');
 const {TEST_DATABASE_URL} = require('../config');
 
 chai.use(chaiHttp);
+
+
+//seed data
+function seedBlogPostData() {
+	console.info('seeding blog post data');
+	const seedData = [];
+
+	for (let i=1; 1<=10; i++) {
+		seedData.push(generateBlogPostData());
+	}
+
+	//Promise
+	return BlogPost.insertMany(seedData);
+}
+
+
+function generateBlogPostData() {
+	return {
+		author: {
+    		firstName: faker.name.firstName(),
+    		lastName: faker.name.lastName()
+    	},
+  		title: faker.lorem.sentence(),
+  		content: faker.lorem.paragraph(),
+  		created: faker.date.past()
+	};
+}
