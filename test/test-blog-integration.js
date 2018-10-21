6b442feedc5b3f3a00bf2fe81b462490207b95e8
Marissa Faulkner <marissagrace808@gmail.com>
2018-10-20 17:49:53 -0700

    added get and post tests

--- test/test-blog-integration.js
+++ test/test-blog-integration.js
@@ -61,20 +61,98 @@ describe('Blog API resource', function() {
 	});
 
 	afterEach(function() {
+		return tearDownDb();
+	});
+
+	after(function() {
 		return closeServer();
 	});
 
 
 	describe('GET endpoints', function() {
 
-		it('should', function() {
+		it('should return all posts', function() {
+			 let res;
+			 return chai.request(app)
+        		.get('/posts')
+       			.then(_res => {
+		          res = _res;
+		          res.should.have.status(200);
+		          res.body.should.have.lengthOf.at.least(1);
+
+		          return BlogPost.count();
+        })
+        .then(count => {
+          res.body.should.have.lengthOf(count);
+        });
+    });
+
+		it('should return posts with right fields', function () {
+      // Strategy: Get back all posts, and ensure they have expected keys
+
+      let resPost;
+      return chai.request(app)
+        .get('/posts')
+        .then(function (res) {
+
+          res.should.have.status(200);
+          res.should.be.json;
+          res.body.should.be.a('array');
+          res.body.should.have.lengthOf.at.least(1);
+
+          res.body.forEach(function (post) {
+            post.should.be.a('object');
+            post.should.include.keys('id', 'title', 'content', 'author', 'created');
+          });
+          resPost = res.body[0];
+          return BlogPost.findById(resPost.id);
+        })
+        .then(post => {
+          resPost.title.should.equal(post.title);
+          resPost.content.should.equal(post.content);
+          resPost.author.should.equal(post.authorName);
+        });
+    });
 
 		});
 	});
 
 	describe('POST endpoints', function() {
 
-		it('should', function() {
+		it('should add a new blog post', function() {
+
+	      const newPost = {
+	        title: faker.lorem.sentence(),
+	        author: {
+	          firstName: faker.name.firstName(),
+	          lastName: faker.name.lastName(),
+	        },
+	        content: faker.lorem.text()
+	      };
+
+	      return chai.request(app)
+	        .post('/posts')
+	        .send(newPost)
+	        .then(function (res) {
+	          res.should.have.status(201);
+	          res.should.be.json;
+	          res.body.should.be.a('object');
+	          res.body.should.include.keys(
+	            'id', 'title', 'content', 'author', 'created');
+	          res.body.title.should.equal(newPost.title);
+	          // cause Mongo should have created id on insertion
+	          res.body.id.should.not.be.null;
+	          res.body.author.should.equal(
+	            `${newPost.author.firstName} ${newPost.author.lastName}`);
+	          res.body.content.should.equal(newPost.content);
+	          return BlogPost.findById(res.body.id);
+	        })
+	        .then(function (post) {
+	          post.title.should.equal(newPost.title);
+	          post.content.should.equal(newPost.content);
+	          post.author.firstName.should.equal(newPost.author.firstName);
+	          post.author.lastName.should.equal(newPost.author.lastName);
+	        });
 
 		});
 	});
